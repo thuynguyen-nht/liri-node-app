8b554ae9b8f3099f9d39824a80e5ea79869fa69a
ThuyN <thuynguyen.nht20@gmail.com>
2020-03-30 18:23:44 -0700

    add omdb

--- liri.js
+++ liri.js
@@ -10,9 +10,6 @@ var moment = require('moment');
 var axios = require("axios");
 
 var nodeArgs = process.argv;
-// var command = process.argv[2];
-
-// var keyWorks = process.argv.split(2);
 
 //Crete an empty variable for holding the artist name
 var artist = "";
@@ -28,6 +25,20 @@ for (var i = 3; i < nodeArgs.length; i++) {
 
     }
 }
+
+var movie = "Mr.Nobody";
+// Loop through all the words in the node argument
+// And do a little for-loop magic to handle the inclusion of "+"s
+for (var i = 3; i < nodeArgs.length; i++) {
+
+    if (i > 3 && i < nodeArgs.length) {
+        movie = movie + "+" + nodeArgs[i];
+    } else {
+        movie += nodeArgs[i];
+
+    }
+}
+
 // Run the axios.get function...
 // The axios.get function takes in a URL and returns a promise (just like $.ajax)
 
@@ -48,7 +59,41 @@ if (nodeArgs[2] === "concert-this") {
                 console.log("Date: " + moment(response.data[i].datetime).format("MM/DD/YYYY"));
                 console.log("===================");
             }
-
+        })
+        .catch(function (error) {
+            if (error.response) {
+                // The request was made and the server responded with a status code
+                // that falls out of the range of 2xx
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                console.log(error.request);
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log("Error", error.message);
+            }
+            console.log(error.config);
+        });
+} else if (nodeArgs[2] === "movie-this") {
+    axios
+        .get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy")
+        .then(function (response) {
+            // If the axios was successful...
+            // Then log the body from the site!
+            // console.log(response.data);
+            console.log("===================");
+            console.log("Title: " + response.data.Title);
+            console.log("Release Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.imdbRating);
+            console.log("Rotten Tomatoes: " + response.data.Ratings[0].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+            console.log("===================");
 
         })
         .catch(function (error) {
@@ -68,4 +113,4 @@ if (nodeArgs[2] === "concert-this") {
             }
             console.log(error.config);
         });
-}
+};
