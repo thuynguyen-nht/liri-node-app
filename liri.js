a1ab3024339f72bcbbf420db796d46048f5765bb
ThuyN <thuynguyen.nht20@gmail.com>
2020-04-01 23:47:16 -0700

    reorganize code

--- liri.js
+++ liri.js
@@ -8,54 +8,68 @@ var moment = require('moment');
 // Grab the axios package...
 var axios = require("axios");
 
+//grap spotify keys
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
+var fs = require("fs")
+
 var nodeArgs = process.argv;
 
-//Crete an empty variable for holding the artist name
-var artist = "";
+// var command = process.argv[2];
 
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-for (var i = 3; i < nodeArgs.length; i++) {
+// var arg2 = process.argv.slice(3).join(" ");
 
-    if (i > 3 && i < nodeArgs.length) {
-        artist = artist + "%20" + nodeArgs[i];
-    } else {
-        artist += nodeArgs[i];
+//=============Switch statment======
 
+
+
+function chooseCommand(command, searchTerm) {
+    switch (command) {
+        case "concert-this":
+            getArtistInfo(searchTerm);
+            break;
+        case "movie-this":
+            getMovieInfo(searchTerm);
+            break;
+        case "spotify-this-song":
+            getSongInfo(searchTerm);
+            break;
+        case "do-what-it-says":
+            doWhatItSays();
+            break;
+        default:
+            console.log('Humm! I do not know that. Let try again.');
     }
+
 }
 
-var movie = "Mr.Nobody";
-// Loop through all the words in the node argument
-// And do a little for-loop magic to handle the inclusion of "+"s
-for (var i = 3; i < nodeArgs.length; i++) {
+function runCommand(userCommand, userSearch) {
 
-    if (i > 3 && i < nodeArgs.length) {
-        movie = movie + "+" + nodeArgs[i];
-    } else {
-        movie += nodeArgs[i];
+    chooseCommand(userCommand, userSearch);
 
-    }
 }
 
-var song = ""
-for (var i = 3; i < nodeArgs.length; i++) {
+//call out runCommand
+runCommand(process.argv[2], process.argv[3])
 
-    if (i > 3 && i < nodeArgs.length) {
-        song = song + "+" + nodeArgs[i];
-    } else {
-        song += nodeArgs[i];
+//=======functions===============
+function getArtistInfo(artist) {
 
+    //Crete an empty variable for holding the artist name
+    var artist = "";
+
+    // Loop through all the words in the node argument
+    // And do a little for-loop magic to handle the inclusion of "+"s
+    for (var i = 3; i < nodeArgs.length; i++) {
+
+        if (i > 3 && i < nodeArgs.length) {
+            artist = artist + "%20" + nodeArgs[i];
+        } else {
+            artist += nodeArgs[i];
+
+        }
     }
-}
-
-// Run the axios.get function...
-// The axios.get function takes in a URL and returns a promise (just like $.ajax)
-
-if (nodeArgs[2] === "concert-this") {
 
     axios
         .get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
@@ -90,7 +104,22 @@ if (nodeArgs[2] === "concert-this") {
             }
             console.log(error.config);
         });
-} else if (nodeArgs[2] === "movie-this") {
+}
+
+function getMovieInfo(movie) {
+
+    var movie = "";
+    // Loop through all the words in the node argument
+    // And do a little for-loop magic to handle the inclusion of "+"s
+    for (var i = 3; i < nodeArgs.length; i++) {
+
+        if (i > 3 && i < nodeArgs.length) {
+            movie = movie + "+" + nodeArgs[i];
+        } else {
+            movie += nodeArgs[i];
+
+        }
+    }
     axios
         .get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy")
         .then(function (response) {
@@ -126,16 +155,71 @@ if (nodeArgs[2] === "concert-this") {
             }
             console.log(error.config);
         });
-} else if (nodeArgs[2] === "spotify-this-song") {
+}
+
+function getSongInfo(song) {
+
+    var song = ""
+    for (var i = 3; i < nodeArgs.length; i++) {
+
+        if (i > 3 && i < nodeArgs.length) {
+            song = song + "+" + nodeArgs[i];
+        } else {
+            song += nodeArgs[i];
+
+        }
+    }
     spotify.request('https://api.spotify.com/v1/search?q=track:' + song + '&type=track&limit=10', function (error, songResponse) {
         if (error) {
             return console.log(error);
         }
         // console.log(songResponse.tracks.items[0]);
-        console.log("Artist: " + songResponse.tracks.items[0].artists[0].name);
-        console.log("Song: " + songResponse.tracks.items[0].name);
-        console.log("URL: " + songResponse.tracks.items[0].preview_url);
-        console.log("Album: " + songResponse.tracks.items[0].album.name);
-    });
+        var songResponses = songResponse.tracks.items;
 
+        for (var i in songResponses) {
+            console.log("Artist: " + songResponses[i].artists[0].name);
+            console.log("Song: " + songResponses[i].name);
+            console.log("URL: " + songResponses[i].preview_url);
+            console.log("Album: " + songResponses[i].album.name);
+            console.log('===============');
+        }
+
+    });
+}
+
+function doWhatItSays() {
+
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+
+        // We will then re-display the content as an array for later use.
+        console.log(dataArr);
+
+        switch (dataArr[0]) {
+            case "concert-this":
+                getArtistInfo(dataArr[1]);
+                break;
+            case "movie-this":
+                getMovieInfo(dataArr[1]);
+                break;
+            case "spotify-this-song":
+                getSongInfo(dataArr[1]);
+                break;
+            case "do-what-it-says":
+                doWhatItSays();
+                break;
+            default:
+                console.log('Humm! I do not know that. Let try again.');
+        }
+
+
+
+    });
 }
